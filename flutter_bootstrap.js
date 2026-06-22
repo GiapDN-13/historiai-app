4c68a4feadaaa6b1cf54d0becfc5ff0769bca923
Dang Nguyen Giap <dangnguyengiap2004@gmail.com>
2026-06-22 23:20:56 +0700

    feat(chat): always-on audio bar; play via backend TTS or browser speech fallback

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4238350109" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3105319128" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
